c66258291b5b5b2e031dddaa65bf311878641864
Dave Wiard <dave.wiard@gmail.com>
2018-04-23 20:28:45 -0700

    Revamped JavaScript
    Revamped JavaScript code to make it function-oriented, more clear what it's doing, and added a POST call attached to submit button click.

--- js/nwea-blog.js
+++ js/nwea-blog.js
@@ -1,3 +1,7 @@
+//
+// json2html template for building the blog post cards consisting of only
+// a title and body text
+//
 let template = {
   '<>':'div', 'class':'col-md-4', 'html':[
     {'<>':'div', 'class':'card', 'id': 'post_id-${id}', 'html': [
@@ -9,14 +13,54 @@ let template = {
 
 
 //
-// create the promise for retreiving stock and chart data and execute
-// on the results
+// Submits the current blog post entry, clears the form, and reloads the blog posts
 //
-$.ajax({
-  url: window.location.protocol + '//' + window.location.host + '/posts'
-}).then(function(data) {
-  $.each( JSON.parse(data), function(index, d) {
-    let html = json2html.transform(d, template);
+function submitPost() {
+  let title = $('input[name=newBlogTitle]').val();
+  let body = $('textarea[name=newBlogBody]').val();
+
+  let saveData = $.ajax({
+        method: "POST",
+        url: window.location.protocol + '//' + window.location.host + '/post/',
+        data: { "title": title, "body": body }
+  }).done(function(response) {
+    let data = { "title": title, "body": body };
+    let html = json2html.transform(data, template);
     $('#allBlogEntries > .row').append(html);
   });
+
+  // TODO
+  // add a .fail() condition on the ajax call above
+}
+
+
+//
+// retrieve all posts from the database and populate cards
+//
+function getPosts() {
+  // clear the current set of blog posts
+  $('#allBlogEntries > .row').empty();
+
+  // create the promise for retreiving stock and chart data and execute
+  // on the results
+  $.ajax({
+    url: window.location.protocol + '//' + window.location.host + '/posts/'
+  }).then(function(data) {
+    $.each(JSON.parse(data), function(index, d) {
+      let html = json2html.transform(d, template);
+      $('#allBlogEntries > .row').append(html);
+    });
+  })
+}
+
+
+//
+// This loads all the blog posts when the page loads
+//
+$(document).ready(function() {
+  // register onClick event for the submit button
+  $('#newBlogSubmit').click(submitPost);
+
+  // load the current set of blog posts
+  getPosts()
 });
