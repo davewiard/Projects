4d1f603ab784bfb86d2ecd01eed8de1b0bb30c65
Dave Wiard <dave.wiard@gmail.com>
2018-07-18 10:09:23 -0700

    Added "Problem" statement and "Answer" comments

--- MongoDB/M121/Chapter.1/solution-hw1.js
+++ MongoDB/M121/Chapter.1/solution-hw1.js
@@ -1,3 +1,20 @@
+//
+// Problem:
+//
+// Help MongoDB pick a movie our next movie night! Based on employee polling,
+// we've decided that potential movies must meet the following criteria.
+// 
+//   * imdb.rating is at least 7
+//   * genres does not contain "Crime" or "Horror"
+//   * rated is either "PG" or "G"
+//   * languages contains "English" and "Japanese"
+//
+// Assign the aggregation to a variable named pipeline
+//
+// Answer:
+//
+//     15
+//
 var pipeline = [
 	{
 		$match: {
--- MongoDB/M121/Chapter.1/solution-hw2.js
+++ MongoDB/M121/Chapter.1/solution-hw2.js
@@ -1,4 +1,43 @@
+//
+// Problem:
+//
+// Our first movie night was a success. Unfortunately, our ISP called to let
+// us know we're close to our bandwidth quota, but we need another movie
+// recommendation!
+//
+// Using the same $match stage from the previous lab, add a $project stage to
+// only display the the title and film rating (title and rated fields).
+//
+// Assign the results to a variable called pipeline.
+//
+// Answer:
+//
+//     15
+//
 var pipeline = [
 	{
+		$match: {
+			'imdb.rating': {
+				$gte: 7
+			},
+			'genres': {
+				$not: {
+					$in: ['Crime', 'Horror']
+				}
+			},
+			'rated': {
+				$in: ['G', 'PG']
+			},
+			'languages': {
+				$all: ['English', 'Japanese']
+			}
+		}
+	},
+	{
+		$project: {
+			'_id': 0,
+			'title': 1,
+			'rated': 1
+		}
 	}
 ];
