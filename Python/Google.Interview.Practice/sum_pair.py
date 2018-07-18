e3746671e167a44a508c75d2537144baed175840
Dave Wiard <dave.wiard@gmail.com>
2018-07-18 16:36:26 -0700

    Enhanced the sum_pair Python script.
    Enhanced the sum_pair Python script to generate random input data.

--- Python/Google.Interview.Practice/sum_pair.py
+++ Python/Google.Interview.Practice/sum_pair.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+'''
+This problem was found online in Google interview question video but was
+also delivered to me by Daily Coding Problem: Problem #1.
+'''
+
+import random
+
 def hasPairWithSum(data, sumToFind):
   '''
   Determine if the current value has a "compliment" which will add up to sumToFind.
@@ -7,16 +14,18 @@ def hasPairWithSum(data, sumToFind):
   comp = set()
   for value in data:
     if value in comp:
-      print('pair: value {}, comp {}'.format(value, sumToFind - value))
-      return True
+      # pair found, return a tuple of the two values
+      return (value, sumToFind - value)
     
     comp.add(sumToFind - value)
 
-  return False
+  # no pair found
+  return None
 
 
 if __name__ == '__main__':
-  data = [1, 2, 4, 6]
+  data = [random.randint(1, 100) for _ in range(100)]
+  print("input =", data)
   sumToFind = 8
-  found = hasPairWithSum(data, sumToFind)
-  print('found = {}'.format(found))
+  pair = hasPairWithSum(data, sumToFind)
+  print('found = {}'.format(pair))
